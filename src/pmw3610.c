2baec8c0e47c3f7c77fcdd282ef7133dadb5b103
Takeshi Yamanaka <64395665+tyamanak@users.noreply.github.com>
2025-01-12 23:57:53 +0900

    change MB1 to LCLK

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -592,7 +592,7 @@ static int automouse_click_listener(const zmk_event_t *eh) {
     if (as_zmk_keycode_state_changed(eh)) {
         struct zmk_keycode_state_changed *kc_ev = as_zmk_keycode_state_changed(eh);
         // state == true (押下時) かつ、keycode が MB1 (マウス左クリック) の場合のみ
-        if (kc_ev->state && kc_ev->keycode == MB1) {
+        if (kc_ev->state && kc_ev->keycode == LCLK) {
             // クリック後は CONFIG_PMW3610_AUTOMOUSE_TIMEOUT_AFTER_CLICK_MS でレイヤーを抜ける設定
             pmw3610_reset_automouse_timer(CONFIG_PMW3610_AUTOMOUSE_TIMEOUT_AFTER_CLICK_MS);
         }
